27bdbca6e36b18222a3ba8e0c63cc4fd74b75bd7
Luan Weber <luanweber98@gmail.com>
2022-08-11 11:34:59 -0300

    Converter PDF para Texto e apresentar em uma tela via .NET C#

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace PdfView
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
---
+++ clsConexao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PdfView
+{
+     class clsConexao
+    {
+        private SqlConnection Conectar()
+        {
+            string strConexao = @"Data Source=DESKTOP-NO90F78\SQLEXPRESS;Initial Catalog=teste;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(strConexao);
+
+            sqlCon.Open();
+
+            return sqlCon;
+   }
+
+        public SqlDataReader Consulta(string cons)
+        {
+            SqlDataReader dtDados;
+            SqlConnection con1 = Conectar();
+            SqlCommand comando = new SqlCommand(cons, con1);
+            dtDados = comando.ExecuteReader();
+
+            return dtDados;
+        }
+        public String InserirDados(String comandoInsercao)
+        {
+            SqlConnection con1 = Conectar();
+            SqlCommand insere = new SqlCommand(comandoInsercao, con1);
+            insere.ExecuteNonQuery();
+
+            return "Inserido com sucesso";
+        }
+
+    }
+}
